4b1886f67389e6274f38b0de98ae16cac1e052ee
egdw <378759617@qq.com>
2017-02-25 15:51:39 +0800

    临时网页加入了文件上传和删除文件时候自动删除本地文件.

--- WebSite/src/com/website/controller/BackgroundManagerController.java
+++ WebSite/src/com/website/controller/BackgroundManagerController.java
@@ -1,8 +1,16 @@
 package com.website.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
@@ -12,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.website.entites.WebsiteFunny;
 import com.website.entites.WebsiteProject;
@@ -21,6 +32,7 @@ import com.website.service.WebSiteFunnyService;
 import com.website.service.WebSiteProjectService;
 import com.website.service.WebSiteTempService;
 import com.website.service.WebSiteUserService;
+import com.website.utils.ZipTools;
 
 @Controller
 @RequestMapping("manager")
@@ -33,6 +45,7 @@ public class BackgroundManagerController {
 	private WebSiteFunnyService funnyService;
 	@Autowired
 	private WebSiteTempService tempService;
+
 	@RequestMapping("manager_person_setting.do")
 	public String entryPersonSetting(Map<String, Object> map) {
 		Subject subject = SecurityUtils.getSubject();
@@ -100,7 +113,7 @@ public class BackgroundManagerController {
 		if (projectId != null && projectUrl != null && projectText != null
 				&& projectText != null) {
 			WebsiteProject project = projectService.getById(projectId);
-			if(project!=null){
+			if (project != null) {
 				project.setProjectText(projectText);
 				project.setProjectTopic(projectTopic);
 				project.setProjectUrl(projectUrl);
@@ -111,7 +124,7 @@ public class BackgroundManagerController {
 		}
 		return "{project_update_error}";
 	}
-	
+
 	/**
 	 * 项目显示界面.进行分页操作
 	 * 
@@ -124,19 +137,17 @@ public class BackgroundManagerController {
 		if (pageNum == null) {
 			pageNum = 0;
 		}
-		ArrayList<WebsiteFunny> list = funnyService
-				.selectProjectByNum(pageNum);
+		ArrayList<WebsiteFunny> list = funnyService.selectProjectByNum(pageNum);
 		map.put("pageNum", String.valueOf(pageNum + 1));
 		int integer = projectService.getPageNum();
 		map.put("allNum", String.valueOf(integer));
 		map.put("projects", list);
 		return "admin/admin_funny_manager";
 	}
-	
+
 	@RequestMapping(value = "manager_funny_add.do", method = RequestMethod.POST)
 	@ResponseBody
-	public String addfunny(
-			@RequestParam("project_topic") String project_topic,
+	public String addfunny(@RequestParam("project_topic") String project_topic,
 			@RequestParam("project_text") String project_text,
 			@RequestParam("project_url") String project_url) {
 		WebsiteFunny project = new WebsiteFunny();
@@ -150,7 +161,7 @@ public class BackgroundManagerController {
 		}
 		return "{add_project_error}";
 	}
-	
+
 	@RequestMapping(value = "manager_funny_delete.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String delFunny(Long projectId) {
@@ -170,7 +181,7 @@ public class BackgroundManagerController {
 		if (projectId != null && projectUrl != null && projectText != null
 				&& projectText != null) {
 			WebsiteFunny project = funnyService.getById(projectId);
-			if(project!=null){
+			if (project != null) {
 				project.setFunnyText(projectText);
 				project.setFunnyTopic(projectTopic);
 				project.setFunnyUrl(projectUrl);
@@ -181,7 +192,7 @@ public class BackgroundManagerController {
 		}
 		return "{project_update_error}";
 	}
-	
+
 	/**
 	 * 项目显示界面.进行分页操作
 	 * 
@@ -194,33 +205,94 @@ public class BackgroundManagerController {
 		if (pageNum == null) {
 			pageNum = 0;
 		}
-		ArrayList<WebsiteTemp> list = tempService
-				.selectProjectByNum(pageNum);
+		ArrayList<WebsiteTemp> list = tempService.selectProjectByNum(pageNum);
 		map.put("pageNum", String.valueOf(pageNum + 1));
 		int integer = projectService.getPageNum();
 		map.put("allNum", String.valueOf(integer));
 		map.put("projects", list);
 		return "admin/admin_temp_manager";
 	}
-	
+
+	@SuppressWarnings({ "deprecation", "deprecation", "resource" })
 	@RequestMapping(value = "manager_temp_add.do", method = RequestMethod.POST)
 	@ResponseBody
-	public String addTemp(
-			@RequestParam("project_topic") String project_topic,
+	public String addTemp(@RequestParam("project_topic") String project_topic,
 			@RequestParam("project_text") String project_text,
-			@RequestParam("project_url") String project_url) {
+			@RequestParam("project_name") String project_name,
+			@RequestParam("project_file") MultipartFile project_file)
+			throws Exception {
 		WebsiteTemp project = new WebsiteTemp();
-		project.setTempText(project_text);
-		project.setTempTopic(project_topic);
-		project.setTempUrl(project_url);
-		project.setTempCreateDate(new Date());
-		boolean b = tempService.insertTemp(project);
-		if (b) {
-			return "{add_project_true}";
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes()).getRequest();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+				"yyyyMMddHHmmssSSS");
+		Subject subject = SecurityUtils.getSubject();
+		String string = simpleDateFormat.format(new Date())
+				+ subject.getPrincipal();
+		String path = request.getRealPath("upload/temp_temp/" + string);
+		File file = new File(path);
+		if (!file.exists()) {
+			file.mkdirs();
 		}
-		return "{add_project_error}";
+		String originalFilename = project_file.getOriginalFilename();
+		if (!originalFilename.substring(originalFilename.lastIndexOf(".")).equals(".zip")) {
+			throw new IllegalArgumentException("format not true");
+		}
+		String contentType = project_file.getContentType();
+		InputStream stream = project_file.getInputStream();
+		byte[] bytes = new byte[(int) project_file.getSize()];
+		int len = -1;
+		File uploadFile = new File(file.getAbsolutePath(), originalFilename);
+		FileOutputStream fileOutputStream = new FileOutputStream(uploadFile);
+		while ((len = stream.read(bytes)) != -1) {
+			fileOutputStream.write(bytes);
+			fileOutputStream.flush();
+		}
+		stream.close();
+		fileOutputStream.close();
+		ZipTools zipTools = new ZipTools();
+		File decompressFile = new File(request.getRealPath("upload/temp/"
+				+ string));
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		zipTools.decompress(uploadFile.getAbsolutePath(),
+				decompressFile.getAbsolutePath(), false);
+		File[] listFiles = decompressFile.listFiles();
+		File indexFile = findIndexFile(listFiles, project_name);
+		if (indexFile != null) {
+			project.setTempText(project_text);
+			project.setTempTopic(project_topic);
+			String string2 = indexFile.getAbsolutePath();
+			int j = string2.lastIndexOf("upload/temp/");
+			project.setTempUrl(indexFile.getAbsolutePath().substring(j));
+			project.setTempCreateDate(new Date());
+			boolean b = tempService.insertTemp(project);
+			if (b) {
+				return "{add_project_true}";
+			}
+			return "{add_project_error}";
+		} else {
+			throw new FileNotFoundException("file is not find");
+		}
 	}
-	
+
+	private File findIndexFile(File[] file, String fileName) {
+		for (int i = 0; i < file.length; i++) {
+			if (file[i].isDirectory()) {
+				File indexFile = findIndexFile(file[i].listFiles(), fileName);
+				if (indexFile != null) {
+					return indexFile;
+				}
+			} else {
+				if (file[i].getName().equals(fileName)) {
+					return file[i];
+				}
+			}
+		}
+		return null;
+	}
+
 	@RequestMapping(value = "manager_temp_delete.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String delTemp(Long projectId) {
@@ -235,15 +307,14 @@ public class BackgroundManagerController {
 
 	@RequestMapping(value = "manager_temp_update.do", method = RequestMethod.POST)
 	@ResponseBody
-	public String updateTemp(Long projectId, String projectUrl,
+	public String updateTemp(Long projectId,
 			String projectText, String projectTopic) {
-		if (projectId != null && projectUrl != null && projectText != null
+		if (projectId != null && projectText != null
 				&& projectText != null) {
 			WebsiteTemp project = tempService.getById(projectId);
-			if(project!=null){
+			if (project != null) {
 				project.setTempText(projectText);
 				project.setTempTopic(projectTopic);
-				project.setTempUrl(projectUrl);
 				project.setTempId(projectId);
 				tempService.updateTemp(project);
 				return "{project_update_success}";
