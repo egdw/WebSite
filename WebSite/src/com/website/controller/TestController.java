220a30a1f56b91d0e01ec6b6598942e2e34d7bad
egdw <378759617@qq.com>
2017-03-02 21:52:37 +0800

    临时测试用的控制器,完成项目后会进行删除

---
+++ WebSite/src/com/website/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+//	@RequestMapping("test")
+//	public String test(){
+//		return "/blog/blog_admin";
+//	}
+}
