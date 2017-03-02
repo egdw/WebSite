a51cc027f9b45707d70605f86f5192002196d763
egdw <378759617@qq.com>
2017-03-02 21:52:57 +0800

    新添加的一些model类

---
+++ WebSite/src/com/website/entites/WebsiteAlbum.java
@@ -0,0 +1,45 @@
+package com.website.entites;
+
+import java.util.Date;
+
+public class WebsiteAlbum {
+    private Integer id;
+
+    private String url;
+
+    private String title;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
